ffe2c4273a6081836a27854abea851b9c61c12ec
Sudharshini56 <bala.sudharshini@gmail.com>
2024-11-17 20:28:33 +0530

    \Centimeters-to-Inches-Converter

---
+++ prompt.js
@@ -0,0 +1,7 @@
+// //Get your input
+
+// let user = prompt("Enter your name: ")
+// console.log("My Name is", user)
+
+let sets = prompt("How many sets do you need: ")
+console.log("you have to pay: ", Number(sets) * 650, "Rs")
